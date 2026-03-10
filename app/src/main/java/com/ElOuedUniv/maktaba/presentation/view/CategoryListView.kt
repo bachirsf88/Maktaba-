b3b08cb6ab3294162b624b0b56e265b52b0605b5
BachirSf88 <bachirsf6@gmail.com>
2026-03-10 23:11:39 +0100

    tp2 + bonus

--- app/src/main/java/com/ElOuedUniv/maktaba/presentation/view/CategoryListView.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/presentation/view/CategoryListView.kt
@@ -74,13 +74,20 @@ fun CategoryList(
     categories: List<Category>,
     modifier: Modifier = Modifier
 ) {
+    Column(modifier = modifier) {
+
+    Text(
+        text = "Total Categories: ${categories.size}",
+        style = MaterialTheme.typography.titleMedium,
+        modifier = Modifier.padding(16.dp)
+    )
     LazyColumn(
         modifier = modifier,
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         items(categories) { category ->
-            CategoryItem(category = category)
+            CategoryItem(category = category)}
         }
     }
 }
@@ -97,9 +104,17 @@ fun CategoryItem(category: Category) {
                 .padding(16.dp)
         ) {
             Text(
-                text = "Category Item",
+                text = category.name,
                 style = MaterialTheme.typography.titleLarge,
                 fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = category.description,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
             )
         }
     }
